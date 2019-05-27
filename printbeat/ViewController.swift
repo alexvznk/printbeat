700ddb20ca3d3be526bc3a578e6c83dfc1d02ace
Alex Vozniuk <alexvznk@gmail.com>
2019-05-26 22:20:18 -0700

    Initial Commit

---
+++ printbeat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  printbeat
+//
+//  Created by Alex on 5/26/19.
+//  Copyright © 2019 Alex Vozniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
